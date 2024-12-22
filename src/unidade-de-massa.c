235fe9229bf675ea6717c641f214605254fcb10d
Breno Tainan Aguiar <brenotaguiar@gmail.com>
2024-12-22 12:56:37 -0300

    criando menu de opcoes de conversao de massa

--- src/unidade-de-massa.c
+++ src/unidade-de-massa.c
@@ -2,5 +2,14 @@
 #include "unidade-de-massa.h"
 
 void converterMassa() {
-    printf("Montando estrutura base");
+    int opcao;
+    double valor;
+
+    printf("\n--- Conversor de Massa ---\n");
+    printf("1. Quilogramas para Gramas\n");
+    printf("2. Quilogramas para Toneladas\n");
+    printf("3. Gramas para Quilogramas\n");
+    printf("4. Toneladas para Quilogramas\n");
+    printf("Escolha uma opção: ");
+    
 }
