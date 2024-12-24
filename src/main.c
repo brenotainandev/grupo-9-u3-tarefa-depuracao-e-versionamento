67d5231794c3934be1b26d5d9525c41b5fee110c
Breno Tainan Aguiar <brenotaguiar@gmail.com>
2024-12-23 22:10:57 -0300

    ajuste na main, lup infinito, e correção do nome do arquivo no makefile

--- src/main.c
+++ src/main.c
@@ -3,40 +3,46 @@
 #include "unidade-de-velocidade.h"
 
 void exibirMenu();
+void limparBufferEntrada();
 
-int main()
-{
+int main() {
     int opcao;
 
-    while (1)
-    {
+    while (1) {
         exibirMenu();
         printf("Escolha uma opção: ");
-        scanf("%d", &opcao);
+        if (scanf("%d", &opcao) != 1) {
+            printf("Entrada inválida. Tente novamente.\n");
+            limparBufferEntrada();
+            continue;
+        }
 
-        switch (opcao)
-        {
-        case 2:
-            converterMassa();
-            break;
-        case 5:
-            converterVelocidade();
-            break;
-        case 0:
-            printf("Saindo do programa.\n");
-            return 0;
-        default:
-            printf("Opção inválida. Tente novamente.\n");
+        switch (opcao) {
+            case 2:
+                converterMassa();
+                break;
+            case 5:
+                converterVelocidade();
+                break;
+            case 0:
+                printf("Saindo do programa.\n");
+                return 0;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
         }
     }
 
     return 0;
 }
 
-void exibirMenu()
-{
+void exibirMenu() {
     printf("\n--- Conversor de Unidades ---\n");
     printf("2. Converter Massa\n");
-    printf("5. Converter Massa\n");
+    printf("5. Converter Velocidade\n");
     printf("0. Sair\n");
+}
+
+void limparBufferEntrada() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
 }
