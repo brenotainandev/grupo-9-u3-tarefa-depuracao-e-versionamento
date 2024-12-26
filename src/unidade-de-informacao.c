22559abdb0dd2d180c6f00048c19e04f9db74abd
Breno Tainan Aguiar <brenotaguiar@gmail.com>
2024-12-26 20:17:58 -0300

    fix print

--- src/unidade-de-informacao.c
+++ src/unidade-de-informacao.c
@@ -5,98 +5,76 @@
 #include <stdlib.h>
 #include "unidade-de-informacao.h"
 
-void converterInformacao()
-{
+void converterInformacao() {
     bool converter = false;
 
-    while(!converter)
-    {
-        int origem, destino; 
-        float valor, resultado; 
-        char *unidades[] = {"Bits", "Bytes", "Kilobytes (KB)", "Megabytes (MB)", "Gigabytes (GB)", "Terabytes (TB)"}; 
-        float conversoes[] = {1, 8, 8192, 8388608, 8589934592, 8796093022208};  // Fatores de conversão para bits 
-        
-        
-        printf("\n--- Conversor de Informação ---\n"); 
-        do 
-        { 
-            printf("Escolha a unidade de origem:\n");
-            
-            for (int i = 0; i < 6; i++) 
-            { 
-                printf("%d. %s\n", i + 1, unidades[i]);
-            } 
-            
-            printf("\n: \n");
-            scanf("%d", &origem); 
-            
-            if (origem < 1 || origem > 6) 
-            { 
-                printf("Opção inválida. Tente novamente.\n");
-            } 
-        } 
-        while (origem < 1 || origem > 6); 
+    while (!converter) {
+        int origem, destino;
+        float valor, resultado;
+        char *unidades[] = {"Bits", "Bytes", "Kilobytes (KB)", "Megabytes (MB)", "Gigabytes (GB)", "Terabytes (TB)"};
+        float conversoes[] = {1, 8, 8192, 8388608, 8589934592, 8796093022208};  // Fatores de conversão para bits
 
-        do 
-        { 
-            ("Escolha a unidade de destino:\n");
-            for (int i = 0; i < 6; i++)
-            {
+        printf("\n--- Conversor de Informação ---\n");
+        do {
+            printf("Escolha a unidade de origem:\n");
+
+            for (int i = 0; i < 6; i++) {
                 printf("%d. %s\n", i + 1, unidades[i]);
-            } 
-            
-            printf("\nOpção: ");
-            scanf("%d", &destino);
-            
-            if (destino < 1 || destino > 6)
-            { 
+            }
+
+            printf("\n: ");
+            scanf("%d", &origem);
+
+            if (origem < 1 || origem > 6) {
                 printf("Opção inválida. Tente novamente.\n");
             }
-        } 
-        while (destino < 1 || destino > 6); 
-            
-        printf("Digite o valor em %s: ", unidades[origem - 1]); 
-            
-        scanf("%f", &valor); 
-            
-        // Converter valor para bits 
-        float valorEmBits = valor * conversoes[origem - 1]; 
-            
-        // Converter de bits para unidade de destino 
-        resultado = valorEmBits / conversoes[destino - 1]; 
+        } while (origem < 1 || origem > 6);
 
-        if (resultado >= 1) 
-        { 
-            printf("\n%.2f %s equivalem a %.2f %s.\n", valor, unidades[origem - 1], resultado, unidades[destino - 1]); 
-        }
-        else
-        { 
+        do {
+            printf("Escolha a unidade de destino:\n");
+            for (int i = 0; i < 6; i++) {
+                printf("%d. %s\n", i + 1, unidades[i]);
+            }
+
+            printf("\nOpção: ");
+            scanf("%d", &destino);
+
+            if (destino < 1 || destino > 6) {
+                printf("Opção inválida. Tente novamente.\n");
+            }
+        } while (destino < 1 || destino > 6);
+
+        printf("Digite o valor em %s: ", unidades[origem - 1]);
+        scanf("%f", &valor);
+
+        // Converter valor para bits
+        float valorEmBits = valor * conversoes[origem - 1];
+
+        // Converter de bits para unidade de destino
+        resultado = valorEmBits / conversoes[destino - 1];
+
+        if (resultado >= 1) {
+            printf("\n%.2f %s equivalem a %.2f %s.\n", valor, unidades[origem - 1], resultado, unidades[destino - 1]);
+        } else {
             printf("\n%.2f %s equivalem a %.6f %s.\n", valor, unidades[origem - 1], resultado, unidades[destino - 1]);
         }
- 
+
         int resposta;
-        do
-        { 
+        do {
             printf("\nDeseja fazer outra conversão?\n");
             printf("1. Sim\n");
             printf("2. Não\n");
-            printf("Opção: \n");
+            printf("Opção: ");
             scanf("%d", &resposta);
-            if (resposta < 1 || resposta > 2)
-            {
+            if (resposta < 1 || resposta > 2) {
                 printf("\nOpção inválida. Tente novamente.\n");
             }
-        } 
-        while (resposta < 1 || resposta > 2);
-        
-        if (resposta == 2) 
-        { 
+        } while (resposta < 1 || resposta > 2);
+
+        if (resposta == 2) {
             converter = true;
-        }
-        else
-        {
+        } else {
             system("cls");
         }
     }
-            
 }
